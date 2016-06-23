03f1206f761efc339f507dabd9e7a3a44847ed67
Matt Grant <nzmattgrant@gmail.com>
2016-06-23 14:52:27 +1200

    Added in some Razor view updates for the card panel. Things are very slow at the moment

--- TrelloIntegration/Services/TrelloService.cs
+++ TrelloIntegration/Services/TrelloService.cs
@@ -9,11 +9,8 @@ using TrelloIntegration.ViewModels;
 
 namespace TrelloIntegration.Services
 {
-    public class Member
-    {
 
-    }
-
+    //TODO add in tests and error handling
     public class TrelloService : ITrelloService
     {
         private const string API_BASE = "https://api.trello.com/1/";
@@ -71,7 +68,20 @@ namespace TrelloIntegration.Services
             var url = string.Format("{0}cards/{1}/actions{2}&filter=commentCard", API_BASE, cardID, getAuthTokenAPIFragment());
             return await getCollectionFromAPIAsync<Comment>(url);
         }
+        //YOu can do this get the basic implementation working thoush since this one requires extracting the data object from the card 
+        //public async Task<IEnumerable<Card>> GetCardsForBoard(string boardID)
+        //{
+        //    var url = string.Format("{0}boards/{1}/cards{2}", API_BASE, boardID, getAuthTokenAPIFragment());
+        //    return await getCollectionFromAPIAsync<Card>(url);
+        //}
 
+        //public async Task<IEnumerable<Comment>> GetCommentsForCard(string cardID)
+        //{
+        //    var url = string.Format("{0}cards/{1}/actions{2}&filter=commentCard", API_BASE, cardID, getAuthTokenAPIFragment());
+        //    return await getCollectionFromAPIAsync<Comment>(url);
+        //}
+
+        //TODO I think we can still do better and get everything by the board id
         //Ideally there would be a way to get all the items associated with a users boards in one hit.
         //I can't see a way to do this so we have to nest calls
         public async Task<IEnumerable<Board>> GetBoardsWithNestedCollectionsForUser(string memberID)
